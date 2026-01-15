59d8ea826fab2c17adc4b9a077496ef0b12b9a9d
Marfernes <fernandesnunes026@gmail.com>
2026-01-15 14:03:03 -0400

    feat(domain): criar entidade Cliente com regras de negócio usando Result

--- Domain/Entidades/Cliente.cs
+++ Domain/Entidades/Cliente.cs
@@ -1,4 +1,5 @@
-﻿using Domain.ValueObject;
+﻿using Application;
+using Domain.ValueObject;
 
 namespace Domain.Entidades
 {
@@ -8,5 +9,25 @@ namespace Domain.Entidades
         public string NomeFantasia { get; private set; }
         public Cnpj Cnpj { get; private set; }
         public bool Ativo { get; private set; }
+
+        private Cliente (string nomeFantasia, Cnpj cnpj) 
+        {
+            Id = Guid.NewGuid();
+            NomeFantasia = nomeFantasia;
+            Cnpj = cnpj;
+            Ativo = true;
+        }
+        public static Result<Cliente> CriarCliente(string nomeFantasia, Cnpj cnpj)
+        {
+            if(string.IsNullOrWhiteSpace(nomeFantasia))
+                return Result<Cliente>.Falha("Nome fantasia é obrigatório.");
+
+           
+            return Result<Cliente>.Ok(new Cliente(nomeFantasia, cnpj));
+        }
+        public void Desativar()
+        {
+            Ativo = false;
+        }
     }
 }
