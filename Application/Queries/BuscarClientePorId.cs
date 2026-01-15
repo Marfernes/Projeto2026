f5b80f9f9452e6a1931707cfc2fa08cd445edda3
Marfernes <fernandesnunes026@gmail.com>
2026-01-15 16:51:28 -0400

    feat: adicionar BuscarClientePorIdQuery e BuscarClientePorIdQueryHandler

---
+++ Application/Queries/BuscarClientePorId.cs
@@ -0,0 +1,3 @@
+﻿namespace Application.Queries;
+
+public record ObterClientePorIdQuery(Guid Id);
---
+++ Application/Queries/BuscarClientePorIdHandler.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Entidades;
+using Domain.Interfaces;
+using Domain.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Application.Queries
+{
+    internal class BuscarClientePorIdHandler
+    {
+        private readonly IClienteRepository _repository;
+
+        public BuscarClientePorIdHandler(IClienteRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Result<Cliente>> Handle(ObterClientePorIdQuery query)
+        {
+            var cliente = await _repository.BuscarPorId(query.Id);
+            if (cliente is null)
+                return Result<Cliente>.Falha("Cliente não encontrado.");
+
+            return Result<Cliente>.Ok(cliente);
+        }
+    }
+}
