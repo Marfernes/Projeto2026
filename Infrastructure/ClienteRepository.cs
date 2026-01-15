1d08452027287f9ddcc59320a39b36a7096d79f5
Marfernes <fernandesnunes026@gmail.com>
2026-01-15 14:39:49 -0400

    feat(infrastructure): repositório de cliente

---
+++ Infrastructure/ClienteRepository.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Entidades;
+using Domain.Interfaces;
+using Domain.ValueObject;
+
+namespace Infrastructure
+{
+    public class ClienteRepository : IClienteRepository
+    {
+        private readonly List<Cliente> _clientes;
+
+        public ClienteRepository(List<Cliente> clientes)
+        {
+            _clientes = clientes;
+        }
+
+        public Task Adicionar(Cliente cliente)
+        {
+            _clientes.Add(cliente);
+            return Task.CompletedTask;
+        }
+
+        public Task<Cliente?> BuscarPorId(Guid id)
+        {
+            return Task.FromResult(_clientes.FirstOrDefault(c => c.Id == id));
+        }
+
+        public Task<bool> ExisteCnpj(Cnpj cnpj)
+        {
+            return Task.FromResult(_clientes.Any(c => c.Cnpj.Equals(cnpj)));
+        }
+    }
+}
