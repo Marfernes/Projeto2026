31ff428a1cee3fb102e697376665ca3805a3fcf0
Marfernes <fernandesnunes026@gmail.com>
2026-01-15 14:08:52 -0400

    feat(domain): adicionar interface IClienteRepository

---
+++ Domain/Interfaces/IClienteRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entidades;
+using Domain.ValueObject;
+
+namespace Domain.Interfaces
+{
+    public interface IClienteRepository
+    {
+        Task Adicionar(Cliente cliente);
+        Task<Cliente?> BuscarPorId(Guid id);
+        Task<bool> ExisteCnpj(Cnpj cnpj);
+    }
+}
